45742c400bc77c146d320b0d17cc59fc1c283fc4
Vladimir Osintsev <osintsev@gmail.com>
2019-09-28 11:12:18 +0300

    Updates

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/healthchecks/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/healthchecks/views.py
@@ -15,16 +15,21 @@ class HealthCheckView(web.View):
     Health checks view.
     """
     async def get(self):
+        data = {}
+        {% if cookiecutter.use_postgres == 'y' %}
         postgres_health = await self._check_postgres()
+        data['postgres'] = postgres_health
+        {% endif %}
+        {% if cookiecutter.use_redis == 'y' %}
         redis_health = await self._check_redis()
+        data['redis'] = redis_health
+        {% endif %}
         uptime = await self._check_uptime()
+        data['uptime'] = uptime
         # Returns JSON-response from dict
-        return web.json_response({
-            'postgres': postgres_health,
-            'redis': redis_health,
-            'uptime': uptime
-        })
+        return web.json_response(data)
 
+    {% if cookiecutter.use_postgres == 'y' %}
     async def _check_postgres(self) -> bool:
         async with self.request.app.postgres.acquire() as conn:
             async with conn.cursor() as cur:
@@ -33,12 +38,15 @@ class HealthCheckView(web.View):
                 async for row in cur:
                     ret.append(row)
                 return ret == [(1,)]
+    {% endif %}
 
+    {% if cookiecutter.use_redis == 'y' %}
     async def _check_redis(self) -> bool:
         """ Checks Redis server key read/write. """
         await self.request.app.redis.set('IS_STARTED', '1')
         redis_health = await self.request.app.redis.get('IS_STARTED')
         return redis_health == '1'
+    {% endif %}
 
     async def _check_uptime(self) -> int:
         """ Server uptime in seconds. """
